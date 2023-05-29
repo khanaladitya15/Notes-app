880b6d2659772c37ea186912d4e67ab4e382f670
khanaladitya15 <khanaladitya15@gmail.com>
2023-05-29 18:04:05 -0400

    note app project initiated.

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,9 +1,13 @@
 const chalk = require('chalk')
 const getNotes = require('./notes')
-const output = getNotes() 
-console.log(output)
 
+const command  = process.argv[2]
+console.log(process.argv)
 
-
-console.log(chalk.white.underline.bold('error!'));
-
+if(command === 'add'){
+    console.log(chalk.green.bold('Adding a new note . . .'))
+}else if(command === 'remove'){
+    console.log(chalk.red.bold('Removing the note.'))
+}else{
+    console.log(chalk.white.bold('Invalid selection ! '))
+}
