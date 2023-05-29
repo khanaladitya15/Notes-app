8ab8736bb8377fda155e6894ecbe2784f3c489fe
khanaladitya15 <khanaladitya15@gmail.com>
2023-05-29 19:12:35 -0400

    yargs commands created.

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,9 +1,48 @@
 const chalk = require('chalk')
+const yargs = require('yargs')
 const getNotes = require('./notes')
 
-const command  = process.argv[2]
-console.log(process.argv)
+// const command  = process.argv[2]
+yargs.version('1.0.1')
 
+// Create add note
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: function(){
+        console.log(chalk.green.bold('Adding a new note . . .'))
+    }
+})
+
+// Create remove note
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a new note',
+    handler: function(){
+        console.log(chalk.red.bold('Removing the note.'))
+    }
+})
+// Create read note
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function(){
+        console.log(chalk.white.bgBlack.bold('Read'))
+    }
+})
+// Create list notes
+yargs.command({
+    command: 'list',
+    describe: 'list a note',
+    handler: function(){
+        console.log(chalk.yellow.bgWhite.bold('List notes'))
+    }
+})
+console.log(yargs.argv)
+
+
+
+/*
 if(command === 'add'){
     console.log(chalk.green.bold('Adding a new note . . .'))
 }else if(command === 'remove'){
@@ -11,3 +50,5 @@ if(command === 'add'){
 }else{
     console.log(chalk.white.bold('Invalid selection ! '))
 }
+*/
+
